18d9c724dd60c334fdcabbac5c53c409f0e38239
bala-amypo <bala@amypo.in>
2025-12-18 09:03:26 +0000

    User commit

--- src/main/java/com/example/demo/controller/ValidationController.java
+++ src/main/java/com/example/demo/controller/ValidationController.java
@@ -3,12 +3,7 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
-
+import jakarta.vali
 
 import com.example.demo.entity.ValidationEntity;
 import com.example.demo.service.ValidationService;
@@ -16,8 +11,8 @@ import java.util.List;
 
 @RestController
 public class ValidationController{
-    @Autowired ValidationService s
-    @PostMapping("vpost")
+    @Autowired ValidationService s;
+    @PostMapping("/vpost")
     public ValidationEntity vpostdata(@Valid @RequestBody ValidationEntity v){
         return s.vpostall(v);
     }
