c38d361638e1f0a98be2f1b353c7fc377096825b
bala-amypo <bala@amypo.in>
2025-12-18 08:23:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -27,13 +27,57 @@ public class ValidationEntity{
     @NotNull
     private int age;
 
+    //Name
+    public String getName(){
+        return username;
+    }
+    public void setName(String username){
+        this.username=username;
+    }
 
+    //Id -primary key
+    public Long getID(){
+        return id;
+    }
+    public void setID(Integer id){
+        this.id=id;
+    }
+
+    //Email
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+
+    // Password
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+
+    // Age
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
 
     public ValidationEntity(
         Long id,
         @NotNull @Size(min=6,max=30,message="Must be 6 to 30") String username,
         @Email(message="Email is not valid") String email,
-         @Size(min=0,max=6,message="Password must contain 6 letters") 
-
-    )
+        @Size(min=0,max=6,message="Password must contain 6 letters") @NotNull(message="Password is mandatory") String password,
+        @Max(30) @Positive @NotNull int age
+    ){
+        this.id=id;
+        this.username=username;
+        this.email=email;
+        this.password=password;
+        this.age=age;
+    }
 }
