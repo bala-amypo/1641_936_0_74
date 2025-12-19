da4bb2c1575b61f1157beee744acc5e890e58514
bala-amypo <bala@amypo.in>
2025-12-19 09:51:28 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentDetails.java
+++ src/main/java/com/example/demo/entity/StudentDetails.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 
-
+@Entity
 public class StudentDetails{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
